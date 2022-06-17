91d88d5701b96b011d36222741ac495c55ec17ab
Sam Sliefert <samsliefert@Sams-Air.lan>
2022-06-16 20:17:27 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the home page baby");brew install git - gui
+})
+app.listen(3000, () => {
+    console.log("WHATSUPPPPP");
+});
