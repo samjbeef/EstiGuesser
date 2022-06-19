69ea86720142f4c48139035934b3de8d69002e35
Sam Sliefert <samjsliefert@gmail.com>
2022-06-19 14:07:54 -0500

    ok

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("happy fathers day dad, youre funnier than mom!!!!");
+    res.send("happy fathers day dad");
 })
 const port = process.env.port || 3000;
 app.listen(port, () => {
