d7732f66ef5b4957982a74f0d211b2cb6ca71cb0
Sam Sliefert <samsliefert@Sams-Air.lan>
2022-06-19 13:45:37 -0500

    fathers day

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the home page baby");
+    res.send("happy fathers day dad, youre funnier than mom!!!!");
 })
 const port = process.env.port || 3000;
 app.listen(port, () => {
