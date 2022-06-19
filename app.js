76f0e8a69706a83f4aceb14d7b0d7ecb1c2eba70
Sam Sliefert <samsliefert@Sams-Air.lan>
2022-06-19 13:36:13 -0500

    change port

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("Welcome to the home page baby");
 })
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("WHATSUPPPPP");
 });
